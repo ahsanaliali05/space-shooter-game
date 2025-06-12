81904f0eb744782cdf6a4b216f4d82bc3fdfcee8
ahsanaliali05 <ahsanali.48636@gmail.com>
2025-06-13 01:43:18 +0500

    ok now player is again edited

--- space shooter game/player.h
+++ space shooter game/player.h
@@ -1,22 +1,34 @@
-// Player.h
 #ifndef PLAYER_H
 #define PLAYER_H
+
+#include "raylib.h"
 #include "Ship.h"
+#include <chrono>
+
+using namespace std::chrono;
 
 class Player : public Ship {
 public:
-    int lives;
+    float maxHealth;
+    float currentHealth;
     int bulletLevel;
+    float speed;
+    bool isSpeedBoosted;
+    steady_clock::time_point speedBoostEndTime;
 
     Player();
     ~Player();
 
     void Update(float dt) override;
     void Draw() const override;
+    void DrawHealthBar() const;
 
-    void AddLife();
+    void TakeDamage();
+    void Heal(float amount);
+    void AddMaxHealth(float amount);
     void UpgradeBullet();
+    void ActivateSpeedBoost(int durationSeconds = 30);
+    bool IsAlive() const;
 };
 
-#endif
-
+#endif // PLAYER_H
--- space shooter game/playerbullet.cpp
+++ space shooter game/playerbullet.cpp
@@ -1,18 +1,72 @@
-#include "PlayerBullet.h"
+#include "Player.h"
+#include "raylib.h"
+using namespace std::chrono;
 
-PlayerBullet::PlayerBullet(Vector2 p) : Bullet(p, 400) {}
-
-void PlayerBullet::Update(float dt)
-{
-    pos.y -= speed * dt;
+Player::Player() : maxHealth(100.0f), currentHealth(100.0f), bulletLevel(1), speed(300.0f), isSpeedBoosted(false) {
+    pos = { 390, 550 };
 }
 
-void PlayerBullet::Draw() const
-{
-    DrawCircleV(pos, 5, YELLOW);
+Player::~Player() {}
+
+void Player::Update(float dt) {
+    float currentSpeed = isSpeedBoosted ? speed * 2 : speed;
+
+    if (isSpeedBoosted && steady_clock::now() >= speedBoostEndTime) {
+        isSpeedBoosted = false;
+    }
+
+    if (IsKeyDown(KEY_LEFT)) pos.x -= currentSpeed * dt;
+    if (IsKeyDown(KEY_RIGHT)) pos.x += currentSpeed * dt;
+    if (IsKeyDown(KEY_UP)) pos.y -= currentSpeed * dt;
+    if (IsKeyDown(KEY_DOWN)) pos.y += currentSpeed * dt;
+
+    if (pos.y > 580) pos.y = 580;
+    if (pos.y < 0) pos.y = 0;
+    if (pos.x < 0) pos.x = 780;
+    if (pos.x > 780) pos.x = 0;
 }
 
-bool PlayerBullet::IsFromPlayer() const
-{
-    return true;
+void Player::Draw() const {
+    DrawRectangleV(pos, { 20, 20 }, BLUE);
+}
+
+void Player::DrawHealthBar() const {
+    float healthBarWidth = 200;
+    float healthPercentage = currentHealth / maxHealth;
+    Color healthColor = healthPercentage > 0.66f ? GREEN :
+        healthPercentage > 0.33f ? YELLOW : RED;
+
+    DrawRectangle(300, 10, healthBarWidth, 20, GRAY);
+    DrawRectangle(300, 10, healthBarWidth * healthPercentage, 20, healthColor);
+    DrawRectangleLines(300, 10, healthBarWidth, 20, WHITE);
+    DrawText(TextFormat("%.0f/%.0f", currentHealth, maxHealth), 310, 12, 16, BLACK);
+}
+
+void Player::TakeDamage() {
+    float damage = maxHealth * 0.33f;
+    currentHealth -= damage;
+    if (currentHealth < 0) currentHealth = 0;
+}
+
+void Player::Heal(float amount) {
+    currentHealth += amount;
+    if (currentHealth > maxHealth) currentHealth = maxHealth;
+}
+
+void Player::AddMaxHealth(float amount) {
+    maxHealth += amount;
+    currentHealth += amount;
+}
+
+void Player::UpgradeBullet() {
+    if (bulletLevel < 3) bulletLevel++;
+}
+
+void Player::ActivateSpeedBoost(int durationSeconds) {
+    isSpeedBoosted = true;
+    speedBoostEndTime = steady_clock::now() + seconds(durationSeconds);
+}
+
+bool Player::IsAlive() const {
+    return currentHealth > 0;
 }
